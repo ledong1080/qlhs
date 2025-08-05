f3333a6d0c82c4263d4f7e27535e29004fde57a4
Your Name <your@email.com>
2025-08-05 12:10:55 +0700

    Cập nhật toàn bộ code mới

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
@@ -16,23 +15,4 @@ export default defineConfig(({ mode }) => {
         }
       }
     };
-=======
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      base: '/qlhs/', // Thêm dòng này để build đúng cho GitHub Pages
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
->>>>>>> b10416c (Initial commit)
 });
